40e08da0d51f5249bb0a45800abab7a77d0949ef
Dan Eversen <daneversen@gmail.com>
2016-09-08 17:05:24 -0400

    add js geocoder script

---
+++ app/public/js/main.js
@@ -0,0 +1,15 @@
+  function geocodeAddress(geocoder) {
+    var address = document.getElementById('address').value;
+    geocoder.geocode({'address': address}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        console.log(results);
+        console.log(results[0].geometry.bounds.H.H);//extracts latitude
+        console.log(results[0].geometry.bounds.j.H);//extracts longitude
+        var lat = results[0].geometry.bounds.H.H;
+        var lng = results[0].geometry.bounds.j.H;
+
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
