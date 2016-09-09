a15a07fcac4ae235f606f8500bf312c8119c5dc2
Dan Eversen <daneversen@gmail.com>
2016-09-09 13:53:51 -0400

    clean up geocoder

--- index.js
+++ index.js
@@ -24,18 +24,9 @@ var options = {
 var geocoder = NodeGeocoder(options);
 
 // Using callback
-geocoder.geocode('29 champs elysée paris', function(err, res) {
+geocoder.geocode('303 w 120th street, new york ny', function(err, res) {
   console.log(res);
 });
-
-// Or using Promise
-geocoder.geocode('29 champs elysée paris')
-  .then(function(res) {
-    console.log(res);
-  })
-  .catch(function(err) {
-    console.log(err);
-  });
 
   response.render('home', {})
 })
