08281b6846f3d04add2c4338dfaf3e6c75370321
Dan Eversen <daneversen@gmail.com>
2016-09-09 13:51:16 -0400

    add geocoder

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,13 @@ $(document).ready(function() {
 
   console.log('script loaded')
 
-/////////////AJAX CALLS//////////////
+
+
+/////////////Geocoder//////////////
+
+
+
+//////////////////NYC OPENDATA///////////////////
 
   var root = 'https://data.cityofnewyork.us/resource/dvh8-u7es.json?$where=within_circle(location_1,%2040.762027813,%20-73.999059288,%20200)and%20occurrence_year%20=%202015';
 
