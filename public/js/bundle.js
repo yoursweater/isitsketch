e68aad9838ae19f985912a0662e70a2fd411e442
Dan Eversen <daneversen@gmail.com>
2017-07-14 18:25:14 -0400

    re enables the input if you enter a bad address

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -3481,14 +3481,11 @@ module.exports = exports['default'];
 /***/ (function(module, exports, __webpack_require__) {
 
 $(document).ready(function() {
-  console.log('webpack bundle initiated')
-  $('#display').fadeToggle(2000, 'linear')
+$('#display').fadeToggle(2000, 'linear')
 
-  ////////////FIREBASE//////////////
-
-  var firebase = __webpack_require__(28);
-
-  var config = {
+////////////FIREBASE//////////////
+var firebase = __webpack_require__(28);
+var config = {
   apiKey: "AIzaSyDXdBaef4C8gaT-LqR7v7jsvgRwJbt7RAs",
   authDomain: "isitsketch.firebaseapp.com",
   databaseURL: "https://isitsketch.firebaseio.com",
@@ -3504,11 +3501,7 @@ function writeRankData(area, rank) {
   });
 }
 
-
-
-
 /////////////Variables//////////////
-
   var mapsToken = "AIzaSyDPMSlU4RW9QMz8ceTsBbBevwtLJvOLDAQ";
   var userAddress;
   var address;
@@ -3518,7 +3511,6 @@ function writeRankData(area, rank) {
   var userAddress = "10 E 21st St, New York NY";
 
 ////////////Form//////////////////
-
   $('.location-form').keypress(function(event){
     if (event.which == 13){
     $(this).find('input').attr('disabled','disabled');
@@ -3527,14 +3519,14 @@ function writeRankData(area, rank) {
         userAddress = $("#input1").val()
         address = userAddress.replace(/\s+/g, '+');
         getMaps(address)
+        event.stopPropagation()
         return false;
     } else {
         return true;
-    }
+      }
   });
 
 ///////////Loading Animation///////
-
 function loadScreen(sketchLevel, robberies, assaults, burglaries, thefts, weapons, topFiveArr) {
   $('#load').fadeTo('slow', 1, function(){
     $('#mainTitle').fadeTo('slow',0)
@@ -3550,9 +3542,7 @@ function loadScreen(sketchLevel, robberies, assaults, burglaries, thefts, weapon
 }
 
 /////////////Display Results/////////////////
-
 function displayResults(sketchLevel, robberies, assaults, burglaries, thefts, weapons, topFiveArr) {
-console.log(topFiveArr)
   $('#newResults').append('<h4 id="feedback" style="color:white;text-align:center;">' + userAddress + '</h4>')
   $("<h2 id='result-level'>Sketch Level: <span class='redText'>"+ sketchLevel +"</span></h2>").appendTo("#newResults")
   $('<div id="indicator"><div id="bar"></div><div id="dial"></div></div>').appendTo("#newResults")
@@ -3564,7 +3554,6 @@ console.log(topFiveArr)
     var currentTopRank = topFiveArr[i]['rank']
     $('#topList').append('<li style="color:white; font-size:1em;">' + currentTopArea + ': ' +'<span id="rankText">' + currentTopRank + '</span>' +'</li>')
   }
-
   var audio = new Audio('sound/gunsound.mp3')
   audio.play();
 
@@ -3586,7 +3575,6 @@ console.log(topFiveArr)
 }
 
 ///////////Maps/////////////////
-
   function getMaps(address){
       $.ajax({
       method: "GET",
@@ -3601,103 +3589,84 @@ console.log(topFiveArr)
   }
 
 //////////////////NYC OPENDATA///////////////////
-
   var getOpendata = function () {
     var root = 'https://data.cityofnewyork.us/resource/57mv-nv28.json?$query=SELECT%20ofns_desc%20WHERE%20within_circle(lat_lon,%20'+myLatitude+',%20'+myLongitude+',%20200)%20AND%20cmplnt_fr_dt%20%3E=%20%272013-12-31T00:00:00%27';
-
     $.ajax({
       dataType: 'json',
       url: root,
       method: 'GET'
     }).then(function(data) {
-            console.log(data)
         if (data[0] != null){
             sketchCalc(data);
         } else{
             alert("Sorry! We didn't recognize that address (New York City addresses only). Try entering it in another format, and be specific.")
+            $('.location-form').find('input').prop('disabled', false);
             }
       });
   }
 
 //////////////SKETCH CALCULATOR////////////////
-
 function sketchCalc(input){
-  var numOffenses = input.length;
-  var rapes = 0;
-  var robberies = 0;
-  var assaults = 0;
-  var burglaries = 0;
-  var thefts = 0;
-  var weapons = 0;
-
-    for(i = 0; i < input.length; i++){
-      if(input[i]['ofns_desc'] === "ROBBERY"){
-            robberies++
-        }
-      if(input[i]['ofns_desc'] === "FELONY ASSAULT"){
-            assaults++
-        }
-      if(input[i]['ofns_desc'] === "BURGLARY"){
-            burglaries++
-        }
-      if(input[i]['ofns_desc'] === "ASSAULT 3 & RELATED OFFENSES"){
-            assaults++
-        }
-      if(input[i]['ofns_desc'] === "PETIT LARCENY"){
-            thefts++
-        }
-      if(input[i]['ofns_desc'] === "GRAND LARCENY"){
-            thefts++
-        }
-      if(input[i]['ofns_desc'] === "DANGEROUS WEAPONS"){
-            weapons++
-        }
-    }
-
-
-  console.log("total robberies: " + robberies)
-  console.log("total assaults: " + assaults)
-  console.log("total burglaries: " + burglaries)
-  console.log("total thefts: " + thefts)
-  console.log("total weapons: " + weapons)
-
-
-  //Methodology:
-
-  //Various offenses are weighted for sketchiness according to my completely arbitrary rating.
-  //However, thefts are in fact negatively correlated with sketchiness.
-  //The reason for this is that although theft is a crime, it tends to occur where
-  //there are greater numbers of peopl (hence more chances to steal) and more stores to rob from.
-  //The presence of stores to rob from is an indication of commerce, and hence,
-  //police presence. Accordingly, despite the high number of thefts in Times Square,
-  //one would be unlikely to say that it's sketchier than Bed-Stuy.
-
-  var sketchLevel = ((robberies*5) + (assaults*9) + (weapons*8) + (burglaries*7) - (thefts))
-
-  console.log("Your sketch level is: " + sketchLevel)
-
-  writeRankData(userAddress,sketchLevel);
-
-  var topFiveRef = firebase.database().ref('sketchRanking/').orderByChild('rank')
-
-  var topFiveArr = []
-  var areaArr = []
-
-  var topFiveSketch = topFiveRef.on('value', function(snap){
-    snap.forEach(function (childsnap){
-      var currentArea = childsnap.val()['area']
-      if (areaArr.indexOf(currentArea) === -1){
-         areaArr.push(currentArea)
-         topFiveArr.unshift(childsnap.val())
-       }
+    var numOffenses = input.length;
+    var rapes = 0;
+    var robberies = 0;
+    var assaults = 0;
+    var burglaries = 0;
+    var thefts = 0;
+    var weapons = 0;
+      for(i = 0; i < input.length; i++){
+        if(input[i]['ofns_desc'] === "ROBBERY"){
+              robberies++
+          }
+        if(input[i]['ofns_desc'] === "FELONY ASSAULT"){
+              assaults++
+          }
+        if(input[i]['ofns_desc'] === "BURGLARY"){
+              burglaries++
+          }
+        if(input[i]['ofns_desc'] === "ASSAULT 3 & RELATED OFFENSES"){
+              assaults++
+          }
+        if(input[i]['ofns_desc'] === "PETIT LARCENY"){
+              thefts++
+          }
+        if(input[i]['ofns_desc'] === "GRAND LARCENY"){
+              thefts++
+          }
+        if(input[i]['ofns_desc'] === "DANGEROUS WEAPONS"){
+              weapons++
+          }
+      }
+      console.log("total robberies: " + robberies)
+      console.log("total assaults: " + assaults)
+      console.log("total burglaries: " + burglaries)
+      console.log("total thefts: " + thefts)
+      console.log("total weapons: " + weapons)
+    //Methodology:
+    //Various offenses are weighted for sketchiness according to my completely arbitrary rating.
+    //However, thefts are in fact negatively correlated with sketchiness.
+    //The reason for this is that although theft is a crime, it tends to occur where
+    //there are greater numbers of peopl (hence more chances to steal) and more stores to rob from.
+    //The presence of stores to rob from is an indication of commerce, and hence,
+    //police presence. Accordingly, despite the high number of thefts in Times Square,
+    //one would be unlikely to say that it's sketchier than Bed-Stuy.
+    var sketchLevel = ((robberies*5) + (assaults*9) + (weapons*8) + (burglaries*7) - (thefts))
+    console.log("Your sketch level is: " + sketchLevel)
+    writeRankData(userAddress,sketchLevel);
+    var topFiveRef = firebase.database().ref('sketchRanking/').orderByChild('rank')
+    var topFiveArr = []
+    var areaArr = []
+    var topFiveSketch = topFiveRef.on('value', function(snap){
+      snap.forEach(function (childsnap){
+        var currentArea = childsnap.val()['area']
+        if (areaArr.indexOf(currentArea) === -1){
+           areaArr.push(currentArea)
+           topFiveArr.unshift(childsnap.val())
+         }
+      })
+     loadScreen(sketchLevel, robberies, assaults, burglaries, thefts, weapons, topFiveArr);
     })
- loadScreen(sketchLevel, robberies, assaults, burglaries, thefts, weapons, topFiveArr);
-  })
-
-}
-
-
-
+ }
 });
 
 
